786d855ca263a0a581eac1c1bef4a36160d5d068
Bernardo-Mattos <bernardomtt2@gmail.com>
2023-05-31 08:12:41 -0300

    adicionando as pastas
    sim

---
+++ Materias/javaScript/primeiroTrim/aula-1/escopo.js
@@ -0,0 +1,10 @@
+const varGlobal = "Essa é uma variavel global"
+
+function mostraConsole(){
+    let varLocal = "Essa é uma variavel local"
+
+    console.log(varGlobal)
+    console.log(varLocal)
+}
+
+mostraConsole()
---
+++ Materias/javaScript/primeiroTrim/aula-1/variaveis.js
@@ -0,0 +1,35 @@
+/*var nome = "bernardo" // declarando variavel
+
+console.log(nome)
+
+var nome = "pedro" // recriando a variavel
+
+console.log(nome)
+
+var nome = "daniel" // reatribuindo o valor
+
+console.log(nome)
+*/
+
+/*
+function getNome(){
+    let nome = 'bernardo'
+
+    if (nome === 'bernardo'){
+        let nome = 'bernardo augusto'
+        console.log('dentro do if ' + nome)
+    }
+
+    console.log('fora do if ' + nome)
+}
+
+getNome()
+*/
+
+const nome = 'benrnardo'
+
+console.log(nome)
+
+const nome = 'pedro'
+
+console.log(nome)
