8f2684d78a83312cd67b916c0c6dbdabf89cefa7
Bernardo-Mattos <bernardomtt2@gmail.com>
2023-05-31 11:53:15 -0300

    criando os objetos

---
+++ Materias/javaScript/primeiroTrim/aula-2/Objetos/objetos.js
@@ -0,0 +1,9 @@
+const variavelzinha = {
+    massa: 'grossona',
+    sabor: 'frango',
+    borda: 'chedar',
+    tempo: 40.1,
+    valor: 50
+}
+
+console.log(variavelzinha)
---
+++ Materias/javaScript/primeiroTrim/aula-2/TiposDeDados/tipos-de-dados.js
@@ -0,0 +1,24 @@
+// alguns tipos de dados com a professora Ana
+
+// string: armazena tudo aquilo que esta entre ''
+const nome = 'bernardo'
+
+// int: armazena numeros
+const idade = 17
+
+// float: armazena numeros com virgula
+const peso = 80.5
+
+// boolean: armazena sim ou nao
+const homem = true
+
+console.log(nome, 'tem', idade, 'e tem', peso, 'quilos')
+
+function concatena(){
+    let homem = true
+    let maior = false
+
+    console.log(`he is a man? ${homem}, and haves 18 yo? ${maior}`)
+}
+
+concatena()
