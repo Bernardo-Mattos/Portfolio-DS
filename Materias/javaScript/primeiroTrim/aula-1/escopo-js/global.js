5b08dca8925057abd8532a61356453b774928f63
Bernardo-Mattos <bernardomtt2@gmail.com>
2023-05-31 08:24:12 -0300

    organizando os arquivos da pasta escopo

---
+++ Materias/javaScript/primeiroTrim/aula-1/escopo-js/global.js
@@ -0,0 +1,12 @@
+const constGlobal = "Essa é uma const global"
+var varGlobal = 'essa é uma variavel global'
+let letGlobal = 'essa é uma let global'
+
+function mostraConsole(){
+    console.log(varGlobal)
+    console.log(constGlobal)
+    console.log(letGlobal)
+
+}
+
+mostraConsole()
