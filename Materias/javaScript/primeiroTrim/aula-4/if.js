e0cc80cbbafe8dea8a19f49f51b9324267dab9d2
Bernardo-Mattos <bernardomtt2@gmail.com>
2023-06-05 09:18:08 -0300

    if's e funtions

---
+++ Materias/javaScript/primeiroTrim/aula-4/if.js
@@ -0,0 +1,15 @@
+console.log('---------- if normal ----------')
+const a = 3
+const b = 2
+
+if (a < b){
+    console.log('a é menor que b')
+}else if(a === b){
+    console.log('else if >>> é verdadeiro')
+}else{
+    console.log('else >>> é falso')
+}
+
+console.log('---------- if ternario ----------')
+
+console.log(a > 8 ? 'verdadeiro' : 'falso')
