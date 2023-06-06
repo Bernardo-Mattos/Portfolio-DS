c1db957ecbcda7726ad63f8213ab58cb839a955d
Bernardo-Mattos <bernardomtt2@gmail.com>
2023-06-06 17:03:43 -0300

    Update NCO.js

--- Materias/bancoDeDados/segundoTrim/juncaoTabelas.sql
+++ Materias/bancoDeDados/segundoTrim/juncaoTabelas.sql
@@ -1,3 +1,10 @@
 CREATE TABLE Clientes(
-    
+    id varchar (3) not null PRIMARY KEY, 
+    nome varchar (50) not null,
+    email VARCHAR(20) not null,
+)
+CREATE TABLE Pedidos(
+    id varchar (3) not null,
+    valor DECIMAL(3,2) not null,
+    FOREIGN KEY (id_cliente) REFERENCES Clientes
 )
