72b127150cee91792466f52664aab37c7cbd0a05
Bernardo-Mattos <bernardomtt2@gmail.com>
2023-06-14 16:14:29 -0300

    primeiro problema

---
+++ Materias/javaScript/primeiroTrim/logic/logic-1.js
@@ -0,0 +1,11 @@
+function returnBigger(num1, num2){
+  console.log('digite o primeiro numero')
+  var num1 = prompt('sim')
+  console.log('digite o primeiro numero')
+  var num2 = prompt('sim')
+  if (num1 > num2) {
+    console.log('O primeiro número é maior que o segundo')
+  }else{}
+}
+
+console.log(returnBigger())
