2bffc55c543e1659eded1f83eaf4066776c555c6
Bernardo-Mattos <bernardomtt2@gmail.com>
2023-06-15 14:23:36 -0300

    sim apenas

--- Materias/javaScript/primeiroTrim/logic/logic-5.js
+++ Materias/javaScript/primeiroTrim/logic/logic-5.js
@@ -15,7 +15,6 @@ function multiplica(num1, num2){
 
 if (num1 < 0 || num2 < 0 ){
   alert('um dos numeros é negativo')
-  multiplica()
 } else {
   let res = multiplica(num1, num2)
   alert ('o resultado é : ' + res )
