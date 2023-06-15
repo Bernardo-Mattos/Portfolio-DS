44f89d5402d91d7b550c8e9cf9968794bebf855b
Bernardo-Mattos <bernardomtt2@gmail.com>
2023-06-15 13:11:39 -0300

    resolvendo o problema 5

---
+++ Materias/javaScript/primeiroTrim/logic/logic-5.js
@@ -0,0 +1,22 @@
+/* 
+  Escreva uma função que recebe dois números inteiros não negativos e realize a
+  multiplicação deles. Porém não pode utilizar o operador de multiplicação.
+*/
+let num1 = parseInt(prompt('digite o primeiro número'))
+let num2 = parseInt(prompt('digite o segundo número'))
+
+function multiplica(num1, num2){
+  let res = 0
+  for ( i = 0; i<num2; i++){
+    res += num1
+  }
+  return res
+}
+
+if (num1 < 0 || num2 < 0 ){
+  alert('um dos numeros é negativo')
+  multiplica()
+} else {
+  let res = multiplica(num1, num2)
+  alert ('o resultado é : ' + res )
+}
