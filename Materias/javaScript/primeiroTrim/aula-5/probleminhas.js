b4bfa248a5d1ad53470effd87acb2ece6512bd58
Bernardo-Mattos <bernardomtt2@gmail.com>
2023-06-26 10:51:53 -0300

    sim

---
+++ Materias/javaScript/primeiroTrim/aula-5/probleminhas.js
@@ -0,0 +1,30 @@
+// primeira
+
+function triangulo (base, altura){
+    return (base * altura) / 2;
+}
+
+console.log(triangulo(2,2))
+
+// segunda
+
+function numeros(numero){
+    return numero.reverse()
+}
+
+console.log(numeros(5))
+
+//terceira 
+
+function vogais(palavra){
+    let vogais = ['a', 'e', 'i', 'o', 'u']
+    let quantidade = 0
+    for (let i = 0; i < palavra.length; i++){
+        if(vogais.includes(palavra[i])){
+            quantidade++
+        }
+    }
+    return quantidade
+}
+
+console.log("a palavra mesa tem: ", vogais('mesa'), 'vogais')
