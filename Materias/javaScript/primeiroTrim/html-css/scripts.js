4e181b5636e5f513ffe561f2f178890201703d76
Bernardo-Mattos <bernardomtt2@gmail.com>
2023-06-28 08:20:08 -0300

    ultimas atualizações

--- Materias/javaScript/primeiroTrim/html-css/scripts.js
+++
@@ -1,8 +0,0 @@
-function hover(element) {
-    element.setAttribute('src', 'Materias/javaScript/primeiroTrim/html-css/imgs/Captura de tela 2023-06-26 111021.png');
-  }
-  
-  function unhover(element) {
-    element.setAttribute('src', 'Materias/javaScript/primeiroTrim/html-css/imgs/mario-low-quality-memes.gif');
-  }
-  
