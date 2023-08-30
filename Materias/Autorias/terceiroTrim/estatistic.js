4d2208f9a5b8c9b3abf96f96126a294c9bea0eb5
Bernardo-Mattos <bernardomtt2@gmail.com>
2023-08-30 07:56:19 -0300

    add dependencies

---
+++ Materias/Autorias/terceiroTrim/estatistic.js
@@ -0,0 +1,20 @@
+function estatistic (){
+  let escolha = prompt("digite 1 se for média\n digite 2 se for mediana \n digite 3 se fo moda")
+  function media(){
+     
+  }
+ 
+  switch(escolha) {
+   case 1:
+     
+      break;
+   case 2:
+     
+      break;
+  case 3:
+     
+      break;
+  default:
+  console.log('digite um número válido')
+}
+}
