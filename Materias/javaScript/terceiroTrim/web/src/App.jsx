1b203f388f0d3733db510bd64de26246931444fe
Bernardo-Mattos <bernardomtt2@gmail.com>
2023-09-11 12:18:31 -0300

    sim

--- Materias/javaScript/terceiroTrim/web/src/App.jsx
+++ Materias/javaScript/terceiroTrim/web/src/App.jsx
@@ -1,17 +1,20 @@
-
 import './App.css';
+import Button from './components/Button';
+import horaCerta from './components/HoraCerta';
 
-function App() {
-  const projeto = 'GreenLearn'
-  
-  // aninha da qbrada me deixou usar o projeto final como att (abandonarei esse)
+function App() {  
   return (
     <div className='app'>
       <header>
-        <h1>{projeto}</h1>
+        <h1>Eu sabo!</h1>
       </header>
       <main>
-
+        <p>
+          mãe eu gostaria de saber o por que eu não sabo ?
+        </p>
+        <img src="https://i.redd.it/xgn9cucxz8hb1.jpg" alt="eu sabo"/>
+        <Button content='sim mamae'/>
+        <horaCerta />
       </main>
     </div>
 
