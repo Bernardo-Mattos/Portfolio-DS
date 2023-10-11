855b4bbcc73b5abdbe4ec433d559e5c2ed562235
Bernardo-Mattos <bernardomtt2@gmail.com>
2023-10-11 09:08:28 -0300

    aaaaaaaa

--- Materias/javaScript/terceiroTrim/BackEnd/index.js
+++ Materias/javaScript/terceiroTrim/BackEnd/index.js
@@ -1,9 +1,47 @@
-const express = require('express')
+const express = require('express');
 
 const server = express();
 
-server.get('./curso' , ()=> {
-  console.log("Curso acessado")
+const cursos = [ 'java' , 'js' , 'c#' , 'c++' ]
+server.use(express.json());
+
+//read
+server.get('/cursos', (req, res) => {
+  return res.json(cursos);
+});
+
+//read
+server.get('/cursos/:index', (req, res) => {
+  const { index } = req.params;
+  return res.json(cursos[index]);
+});
+
+//create
+server.post('/cursos', (req , res)=>{
+  const { name } = req.body
+  cursos.push(name)
+  return res.send(cursos)
 })
 
-server.listen(3001)
+server.put('/cursos/:index', (req , res)=>{
+  const{ index }  = req.params
+  const { name } = req.body
+
+  cursos[index] = name;
+  return res.json(cursos);
+});
+
+//delete
+server.delete('/cursos/:index', (req, res) => {
+  const { index } = req.params;
+  // Verifique se o índice é um número válido
+  if (isNaN(index) || index < 0 || index >= cursos.length) {
+    return res.status(400).json({ error: 'Índice inválido' });
+  }
+  // Use splice para remover o elemento no índice especificado
+  cursos.splice(index, 1);
+
+  return res.json(cursos);
+});
+
+server.listen(3001);
